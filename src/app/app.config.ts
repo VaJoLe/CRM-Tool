c607937e8bedb8ab83a2bb806c9f9da4828ddac9
Jonas Valtin <138028521+VaJoLe@users.noreply.github.com>
2025-05-12 00:46:24 +0200

    feat: integrate Firebase and enhance user dialog
    - Added @angular/fire package for Firebase integration.
    - Configured Firebase app initialization in app.config.ts.
    - Updated dialog-add-user component to bind user input fields to a User model.
    - Implemented saveUser method to handle user data submission.
    - Created User class to manage user properties.
    - Improved user dialog UI with proper placeholders and bindings.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,11 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"simple-crm-a3de0","appId":"1:382907753108:web:358433fc99ead98d0f0a81","storageBucket":"simple-crm-a3de0.firebasestorage.app","apiKey":"AIzaSyB6Y4__Fv9p918_M7SiOsuhvWnhOtd5n7Q","authDomain":"simple-crm-a3de0.firebaseapp.com","messagingSenderId":"382907753108"})), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideStorage(() => getStorage())]
 };
--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -3,9 +3,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {provideNativeDateAdapter} from '@angular/material/core';
-
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { User } from '../../models/user.class';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -17,8 +18,17 @@ import {provideNativeDateAdapter} from '@angular/material/core';
     MatFormFieldModule,
     MatInputModule,
     MatDatepickerModule,
+    FormsModule,
   ],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss',
 })
-export class DialogAddUserComponent {}
+export class DialogAddUserComponent {
+  user = new User();
+  birthDate!: Date;
+
+  saveUser() {
+    this.user.birthDate = this.birthDate.getTime();
+    console.log('current user is', this.user);
+  }
+}
